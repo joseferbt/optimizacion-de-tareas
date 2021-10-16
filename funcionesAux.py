529b7884beb0b34ec88bb52522d04d8e9d653db0
Jose Fernando Botina <joseferbt@gmail.com>
2021-10-16 16:12:04 -0500

    ingresa funcionando

--- funcionesAux.py
+++ funcionesAux.py
@@ -4,24 +4,28 @@ def mostrar(matriz):
             print("\t", valor, end=" ")
         print()
 
-def printAns(ans,file):
-    aux =0
+
+def printAns(ans, file):
+    aux = 0
     for x in ans:
-        aux+= int(x[2])-int(x[1])
+        aux += int(x[2]) - int(x[1])
     file.write(str(len(ans)))
-    file.write("\n"+str(aux))
+    file.write("\n" + str(aux))
     for x in ans:
-        file.write("\n"+x[0])
+        file.write("\n" + x[0])
+
 
 def beneficio(arr):
-    return int(arr[2])-int(arr[1])
-def countSort(arr,horasD,n):
-    output = [["","",""] for a in range(n)]
-    count = [0 for b in range(horasD+1)]
-    ans= [["","",""] for c in range(n)]
+    return int(arr[2]) - int(arr[1])
+
+
+def countSort(arr, horasD, n):
+    output = [["", "", ""] for a in range(n)]
+    count = [0 for b in range(horasD + 1)]
+    ans = [["", "", ""] for c in range(n)]
     for i in arr:
         count[int(i[2])] += 1
-    for x in range(1,horasD+1):
+    for x in range(1, horasD + 1):
         count[x] += count[x - 1]
     for y in range(n):
         output[count[int(arr[y][2])] - 1] = arr[y]
@@ -29,45 +33,41 @@ def countSort(arr,horasD,n):
     for z in range(len(arr)):
         ans[z] = output[z]
     return ans
-def entre(ar,gl):
-    return (False,True)[(ar[1] <= gl[1]) and (gl[1] < ar[2])]
-def ingresa(arr,inter,newinter):
-    l = len(inter)-1
-    aux = bool
-    count = 0
-    if l ==-1:
+
+
+def entre(ar, gl):
+    return (False, True)[(ar[1] <= gl[1]) and (gl[1] < ar[2])]
+
+
+def ingresa(arr, inter, newinter):
+    l = len(inter) - 1
+    if l == -1:
         return False
     else:
-        if int(arr[1])>=inter[l][0]and int(arr[2])<=inter[l][1]:
-            if inter[l][0]==int(arr[1]) and inter[l][1]==int(arr[2]):
-                inter.pop()
-                newinter.append(inter)
-                return True
+        if int(arr[1]) >= inter[l][0] and int(arr[2]) <= inter[l][1]:
+            if inter[l][0] == int(arr[1]) and inter[l][1] == int(arr[2]):
+                print()
             else:
-                if inter[l][0]==int(arr[1]) and inter[l][1]>int(arr[2]):
-                    print([int(arr[2]),inter[l][1]],"here")
-                    newinter.append([int(arr[2]),inter[l][1]])
-                    print(newinter,"[5]")
-                elif inter[l][0]<int(arr[1]) and inter[l][1]>int(arr[2]):
+                if inter[l][0] == int(arr[1]) and inter[l][1] > int(arr[2]):
+                    newinter.append([int(arr[2]), inter[l][1]])
+                elif inter[l][0] < int(arr[1]) and inter[l][1] > int(arr[2]):
                     newinter.append([inter[l][0], int(arr[1])])
                     newinter.append([int(arr[2]), inter[l][1]])
-                elif inter[l][0]<int(arr[1]) and inter[l][1]==int(arr[2]):
+                elif inter[l][0] < int(arr[1]) and inter[l][1] == int(arr[2]):
                     newinter.append([inter[l][0], int(arr[1])])
             inter.pop()
-            print(inter,"[2]")
             for x in range(l):
-                # print(x,"[1]")
                 newinter.append(inter[x])
-            print(newinter,"[6]")
+            print(newinter,"[X]")
             return True
         else:
-            print(newinter,"[4]")
             newinter.append(inter.pop())
-            print(newinter,"[3]")
-            print(inter, [0])
-            return ingresa(arr,inter,newinter)
-
-
+            return ingresa(arr, inter, newinter)
+def newbeneficio(intervalos):
+    count =0
+    for i in intervalos:
+        count += i[1]-i[0]
+    return count
 """intervalo=[ x for x in range(0,24) ]
 def ingresados(arr,  newinter):
     if (arr[1] in intervalo) and (arr[2] in intervalo):
@@ -77,4 +77,3 @@ def ingresados(arr,  newinter):
     else:
         return False
 """
-
--- main.py
+++ main.py
@@ -17,16 +17,19 @@ mostrar(matriz)                        #-------------------------------------
 def selActi_1(n, arr):
     ben = [[0 for x in range(horasDisponibles[1] + 1)] for x in range(n + 1)]
     sol = [[0 for x in range(horasDisponibles[1] + 1)] for x in range(n + 1)]
-    available=horasDisponibles
+    available=[horasDisponibles]
     for i in range(1, n + 1):
         for c in range(horasDisponibles[1] + 1):
             if beneficio(arr[i - 1]) > c:
                 ben[i][c] = ben[i - 1][c]
                 sol[i][c] = 0
             else:
-                if ingresa(arr[i],available):
+                aux=[]
+                if ingresa(arr[i-1],[[0,c]],aux):
+                    available=aux
                     ben[i][c] = max(ben[i - 1][c], beneficio(arr[i - 1]) + ben[i - 1][c - beneficio(arr[i - 1])])
                     sol[i][c] = 1
+                print(c,arr[i-1],available,"[",i,"]");
     return sol
 
 
@@ -40,14 +43,24 @@ def solActi_1(arr, n, c, mat, aux):
         else:
             solActi_1(arr, n - 1, c, mat, aux)
     return aux
-mataux = []
+"""mataux = []
 inter = [[0,24]]
-
-
-# printAns(solActi_1(selActi_1(n, matriz), n, horasDisponibles, matriz, mataux), outFile)
+ingresa(["a","2","3"],inter,mataux)
+print(mataux)
+ingresa(["a","5","8"],mataux,inter)
+print(inter)
+mataux=[]
+ingresa(["a","15","17"],inter,mataux)
+print(mataux)
+inter=[]
+ingresa(["a","8","15"],mataux,inter)
+print(inter)"""
+mataux=[]
+# printAns(solActi_1(selActi_1(n, matriz), n, horasDisponibles[1], matriz, mataux), outFile)
 # mostrar(selActi_1(n, matriz))
 # for x in range(n):
 #     print(matriz[x][0]," ",matriz[x][1]," ",matriz[x][2])
 
+print(newbeneficio([[0,1],[4,8],[11,24]]))
 ioFile.close()
 outFile.close()
