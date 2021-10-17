9fb4030dde3e5caf03ea6e35a00def9c12f0a068
HusskyAngel <angel.juan@correounivalle.edu.co>
2021-10-17 17:54:44 -0500

    solucion ingenua 2

--- ordenar.py
+++ ordenar.py
@@ -23,4 +23,19 @@ def mergeSort(array):
             j += 1
             k += 1
 
+def countSort(arr, horasD, n):
+    output = [["", "", ""] for a in range(n)]
+    count = [0 for b in range(horasD + 1)]
+    ans = [["", "", ""] for c in range(n)]
+    for i in arr:
+        count[int(i[2])] += 1
+    for x in range(1, horasD + 1):
+        count[x] += count[x - 1]
+    for y in range(n):
+        output[count[int(arr[y][2])] - 1] = arr[y]
+        count[int(arr[y][2])] -= 1
+    for z in range(len(arr)):
+        ans[z] = output[z]
+    return ans
+
 
